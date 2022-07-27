05d732c1980048bea8a3490dfac07bdfc8a824cb
Cesar Gustavo <csargustavo.cg@gmail.com>
2022-07-27 16:28:56 -0300

    exercicios add

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,6 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const semEpaço = minhaString.trim();
+console.log(`a string possui ${minhaString.length} caracteres, apos remover os espaços fica ${semEpaço.length}`)
+
+const novaString = minhaString.replaceAll("_______","stilicioso")
+console.log(novaString);
